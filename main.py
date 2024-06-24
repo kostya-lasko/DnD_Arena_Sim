e6a98e3c7568c8ee95b07be05b7895e2c1f3a915
KL <k.lasko.minsk@gmail.com>
2024-06-24 23:00:07 +0200

    learned about the f-string, so I refactored all print commands.

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def clear():
 def charStatsMod(charClass):
   charClass = charClass.lower()
   if charClass in ["warrior"]:
-    healthMod = 2
+    healthMod = 2.2
     attackBonus = 3
     range = 0
     magicChance = 0
@@ -52,7 +52,7 @@ def magicBurst(characterMagicChance):
       magicHealing = 0
     if magicType == 2:
       magicDamage = 0
-      magicHealing = random.randint(1,3) + 1
+      magicHealing = random.randint(1,3)
     return (magicDamage, magicHealing)
   else:
     return 0, 0 
@@ -60,34 +60,33 @@ def magicBurst(characterMagicChance):
 
 #this subroutine lets your create your character     
 def characterGenerator():
- # while True:
-    characterName = input ("Name your character: ")
-    time.sleep(0.1)
-    characterClass = input ("Select your character class: (warrior, mage, ranger) ")
-    time.sleep(0.2)
-    healthMod, attackBonus, range, magicChance = charStatsMod(characterClass)
-    
-    # setting character stats: health, str, range and magic chance
-    characterHealth = int(((random.randint(2,6) + random.randint(2,6))*healthMod + 20))
-    characterStr = int((random.randint(1,3) + attackBonus))
-    characterRange = int(range)
-    #characterAttackBonus = int(attackBonus)
-    characterMagicChance = float(magicChance)
-    print ("Generating awesome stats for this amazing character... ")
-    time.sleep(1)
-    print (Colors.OKCYAN + "Your character's name is", characterName)
-    time.sleep(0.33)
-    print ("Your character's class is", characterClass)
-    time.sleep(0.33)
-    print ("HP: " + str(characterHealth))
-    time.sleep(0.33)
-    print ("STR: " + str(characterStr))
-    time.sleep(0.33)
-    print ("Attack range: " + str(characterRange))
-    time.sleep(0.33)
-    print ("Magic Chance: " + str(characterMagicChance) + Colors.ENDC)
-    time.sleep(0.33)
-    return characterName, characterClass, characterHealth, characterStr, characterRange, characterMagicChance
+  characterName = input ("Name your character: ")
+  time.sleep(0.1)
+  characterClass = input ("Select your character class: (warrior, mage, ranger) ").lower().strip()
+  time.sleep(0.2)
+  healthMod, attackBonus, range, magicChance = charStatsMod(characterClass)
+  
+  # setting character stats: health, str, range and magic chance
+  characterHealth = int(((random.randint(2,6) + random.randint(2,6))*healthMod + 20))
+  characterStr = int((random.randint(1,3) + attackBonus))
+  characterRange = int(range)
+  #characterAttackBonus = int(attackBonus)
+  characterMagicChance = float(magicChance)
+  print ("Generating awesome stats for this amazing character... ")
+  time.sleep(1)
+  print (Colors.OKCYAN + f"Your character's name is {characterName}")
+  time.sleep(0.33)
+  print (f"Your character's class is {characterClass}")
+  time.sleep(0.33)
+  print (f"HP: {characterHealth}")
+  time.sleep(0.33)
+  print (f"STR: {characterStr}")
+  time.sleep(0.33)
+  print (f"Attack range:  {characterRange}")
+  time.sleep(0.33)
+  print (f"Magic Chance: {characterMagicChance}" + Colors.ENDC)
+  time.sleep(0.33)
+  return characterName, characterClass, characterHealth, characterStr, characterRange, characterMagicChance
 
 
 # subroutine that handles the combat system
@@ -95,7 +94,7 @@ def combat (char1Name, char1Health, char1Str, char1Range, char1MagicChance, char
   print ("The fight begins! Characters start at the opposite corners of the arena.")
   distance = 5
   round = 0
-  while True:
+  while char1Health > 0 and char2Health > 0:
     char1Dmg = random.randint(1,3) + char1Str
     char2Dmg = random.randint(1,3) + char2Str
     if distance > 0:
@@ -103,29 +102,29 @@ def combat (char1Name, char1Health, char1Str, char1Range, char1MagicChance, char
       distance = distance - 1
     #check for ranged attacks
       if char1Range > distance:
-        print (char1Name + " attacks at range dealing " + str(char1Str) + " damage")
+        print (f"{char1Name} attacks at range dealing {char1Str} damage")
         char2Health = char2Health - char1Dmg
         time.sleep(0.5)
       if char2Range > distance:
-        print (char2Name + " attacks at range dealing " + str(char2Str) + " damage")
+        print (f"{char2Name} attacks at range dealing {char2Str} damage")
         char1Health = char1Health - char2Dmg
         time.sleep(0.5)
     
     # check for magic bursts
     char1MagicDamage, char1MagicHealing = magicBurst(char1MagicChance)
     if char1MagicDamage > 0:
-      print (Colors.OKCYAN + char1Name + " magic surge goes wild dealing " + str(char1MagicDamage) + " damage" + Colors.ENDC)
+      print (Colors.OKCYAN + f"{char1Name} magic surge goes wild dealing {char1MagicDamage} damage" + Colors.ENDC)
       char2Health = char2Health - char1MagicDamage
       time.sleep(1)
     if char1MagicHealing > 0:
-      print (Colors.OKCYAN+ "Magic energies gather around " + char1Name + " healing them for " + str(char1MagicHealing) + " health" + Colors.ENDC)
+      print (Colors.OKCYAN + f"Magic energies gather around {char1Name} healing them for {char1MagicHealing} health" + Colors.ENDC)
       char1Health = char1Health + char1MagicHealing
     char2MagicDamage, char2MagicHealing = magicBurst(char2MagicChance)
     if char2MagicDamage > 0:
-      print (Colors.OKCYAN + char2Name + " explodes with magic dealing " + str(char2MagicDamage) + " damage" + Colors.ENDC)
+      print (Colors.OKCYAN + f"{char2Name} explodes with magic dealing {char2MagicDamage} damage" + Colors.ENDC)
       char1Health = char1Health - char2MagicDamage
     if char2MagicHealing > 0:
-      print (Colors.OKCYAN + "Magic swirls around " + char2Name + " healing them for " + str(char2MagicHealing) + " health" + Colors.ENDC)
+      print (Colors.OKCYAN + f"Magic swirls around {char2Name} healing them for {char2MagicHealing} health" + Colors.ENDC)
       char2Health = char2Health + char2MagicHealing
       time.sleep(1)
 
@@ -133,13 +132,13 @@ def combat (char1Name, char1Health, char1Str, char1Range, char1MagicChance, char
     # melee combat
     if distance == 0:
       print (Colors.BOLD  + "Both combatants clash in melee!" + Colors.ENDC)
-      print (char1Name + " attacks dealing " + str(char1Dmg) + " damage")
+      print (f"{char1Name} attacks dealing {char1Dmg} damage")
       char2Health = char2Health - char1Dmg
       time.sleep(0.5)
-      print (char2Name + " hits back dealing " + str(char2Dmg) + " damage")
+      print (f"{char2Name} hits back dealing {char2Dmg} damage")
       char1Health = char1Health - char2Dmg
-      #print (char1Name + " has " + Colors.OKGREEN + str(char1Health) + Colors.ENDC + " health remaining")
-      #print (char2Name + " has " + Colors.OKGREEN + str(char2Health) + Colors.ENDC + " health remaining")
+      #print (f"{char1Name} has " + Colors.OKGREEN + f"{char1Health}" + Colors.ENDC + " health remaining")
+      #print (f"{char2Name} has " + Colors.OKGREEN + f"{char2Health}" + Colors.ENDC + " health remaining")
       #print ()
 
     
@@ -150,15 +149,15 @@ def combat (char1Name, char1Health, char1Str, char1Range, char1MagicChance, char
     if char1Health <= 0:
       print (Colors.FAIL + char1Name + " falls on the ground" + Colors.ENDC)
       if char2Health > 0:
-        print (char2Name + " stands victorious!")
-        print (char2Name + " has " + Colors.OKGREEN + str(char2Health) + Colors.ENDC + " health remaining")
+        print (f"{char2Name} stands victorious!")
+        print (f"{char2Name} has " + Colors.OKGREEN + f"{char2Health}" + Colors.ENDC + " health remaining")
         break
     elif char2Health <= 0:
-      print (Colors.FAIL + char2Name + " falls on the ground" + Colors.ENDC)
+      print (Colors.FAIL + f"{char2Name} falls on the ground" + Colors.ENDC)
       
       if char1Health > 0:
-        print (char1Name + " wins this fight!")
-        print (char1Name + " has " + Colors.OKGREEN + str(char1Health) + Colors.ENDC + " health remaining")
+        print (f"{char1Name} wins this fight!")
+        print (f"{char1Name} has " + Colors.OKGREEN + f"{char1Health}" + Colors.ENDC + " health remaining")
         break    
     
     round += 1   
